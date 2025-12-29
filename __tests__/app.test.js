2d8d5860afd7d5f9b5e3fff9930712236a549d98
Prostik <artem.abillov@gmail.com>
2025-12-30 00:47:41 +1000

    first commit

---
+++ __tests__/app.test.js
@@ -0,0 +1,7 @@
+const request = require('supertest');
+const app = require('../app');  // Импорт вашего приложения
+it('responds with Hello, World!', async () => {
+  const response = await request(app).get('/'); // Просто вызываем app, чтобы получить запрос
+  expect(response.statusCode).toBe(200);
+  expect(response.text).toBe('Hello, World!\n');
+});
